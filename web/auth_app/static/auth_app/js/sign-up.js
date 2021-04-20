a6a54d474b20aa90f44272e5df472e59c42831fc
SilinAlexander <asilin1997@mail.ru>
2021-04-20 21:53:33 +0500

    2

--- web/auth_app/static/auth_app/js/sign-up.js
+++ web/auth_app/static/auth_app/js/sign-up.js
@@ -6,4 +6,33 @@ $(function(){
 function signUp(event){
   event.preventDefault();
   console.log('click')
+  let form = $(this);
+  let path = form.attr('action')
+  console.log(path)
+  let data = form.serialize()
+  console.log(data)
+  $.ajax(
+  {
+    url : path,
+    type : 'post',
+    data : data,
+    success : function(data){
+      console.log(data)
+    },
+    error : function(data){
+      console.log(data, 'error')
+      $('.help-block').remove()
+      if (typeof data.responseJSON.first_name !== 'undefined'){
+      $('#firstNameGroup').addClass('has-error')
+      $('#firstNameGroup').append(
+        "<div class='help-block'>"+ data.responseJSON.first_name +"</div>"
+      )
+      }
+
+
+
+
+    }
+  }
+  )
 }
